0fa993c7f692f7a153bcc7a19539227d5bfc6024
maksym-gryb <maksym.gryb@gmail.com>
2025-03-15 20:55:13 -0700

    feat(api): database available in app-state

--- api/src/main.rs
+++ api/src/main.rs
@@ -3,27 +3,16 @@
 use rocket::serde::{json::Json, Deserialize, Serialize};
 
 use rocket::get;
-use rocket_okapi::{openapi, openapi_get_routes, swagger_ui};
+use rocket::State;
+use rocket_okapi::{openapi, openapi_get_routes};
 use rocket_okapi::swagger_ui::{make_swagger_ui, SwaggerUIConfig};
+use rocket_okapi::okapi::schemars;
 use rocket_okapi::okapi::schemars::JsonSchema;
 
-use rocket::form::FromForm;
-use rocket_okapi::okapi::schemars;
-use rocket_okapi::settings::UrlObject;
-
 use rocket::http::Method;
-use rocket_cors::{AllowedHeaders, AllowedMethods, AllowedOrigins, CorsOptions};
+use rocket_cors::{AllowedHeaders, AllowedOrigins, CorsOptions};
 
-use sqlx::{migrate::MigrateDatabase, Sqlite, SqlitePool, FromRow, Row};
-
-
-
-#[derive(Serialize, Deserialize, JsonSchema)]
-struct Item {
-    id: i32,
-    name: String,
-    price: f64,
-}
+use sqlx::{migrate::MigrateDatabase, Sqlite, SqlitePool, FromRow};
 
 #[derive(Serialize, Deserialize, JsonSchema, Clone, FromRow, Debug)]
 struct Event {
@@ -33,34 +22,25 @@ struct Event {
     end_date: String
 }
 
+struct AppState{
+    db : SqlitePool
+}
+
 const DB_URL: &str = "sqlite://sqlite.db";
 
 #[openapi(tag = "Events")]
 #[get("/events")]
-async fn get_events() -> Json<Vec<Event>> {
-    let db = SqlitePool::connect(DB_URL).await.unwrap();
-
+async fn get_events(state : &State<AppState>) -> Json<Vec<Event>> {
     let users = sqlx::query_as::<_, Event>(
         "SELECT * FROM events"
-    ).fetch_all(&db)
+    ).fetch_all(&state.db)
     .await
     .unwrap();
 
     return Json(users);
 }
 
-#[openapi(tag = "Items")]
-#[get("/items")]
-fn get_items() -> Json<Vec<Item>> {
-    Json(vec![
-        Item { id: 1, name: "Item 1".to_string(), price: 100.0 },
-        Item { id: 2, name: "Item 2".to_string(), price: 200.0 },
-    ])
-}
-
 fn get_docs() -> SwaggerUIConfig {
-    use rocket_okapi::settings::UrlObject;
-
     SwaggerUIConfig {
     url: "../openapi.json".to_string(),
     ..Default::default()
@@ -70,7 +50,6 @@ fn get_docs() -> SwaggerUIConfig {
 #[launch]
 #[tokio::main]
 async fn rocket() -> _ {
-
     if !Sqlite::database_exists(DB_URL).await.unwrap_or(false) {
         println!("Creating database {}", DB_URL);
         match Sqlite::create_database(DB_URL).await {
@@ -83,18 +62,20 @@ async fn rocket() -> _ {
 
     let db = SqlitePool::connect(DB_URL).await.unwrap();
 
-    let result = sqlx::query("
+    let create_table_result = sqlx::query("
         CREATE TABLE IF NOT EXISTS events
         (id INTEGER PRIMARY KEY NOT NULL, title TEXT NOT NULL, start_date TEXT NULL, end_date TEXT NULL);"
     ).execute(&db).await.unwrap();
 
-    println!("Create events table result: {:?}", result);
+    println!("Create events table result: {:?}", create_table_result);
 
-    let truncateResult = sqlx::query("
+    let delete_result = sqlx::query("
         DELETE FROM events
     ").execute(&db).await.unwrap();
 
-    let insertResult = sqlx::query("
+    println!("Delete result: {:?}", delete_result);
+
+    let insert_result = sqlx::query("
         INSERT INTO events
         (title, start_date, end_date)
         VALUES
@@ -102,13 +83,10 @@ async fn rocket() -> _ {
         ('sacrifice', date('now', '-2 day'), date('now', '+5 day'))
     ").execute(&db).await.unwrap();
 
-    println!("Inserts result: {:?}", insertResult);
-
-
-    let allowed_origins = AllowedOrigins::some_exact(&["http://localhost:5173"]);
+    println!("Inserts result: {:?}", insert_result);
 
     let cors = CorsOptions {
-        allowed_origins,
+        allowed_origins: AllowedOrigins::some_exact(&["http://localhost:5173"]),
         allowed_methods: vec![Method::Get, Method::Post].into_iter().map(From::from).collect(),
         allowed_headers: AllowedHeaders::some(&["Authorization", "Accept"]),
         allow_credentials: true,
@@ -117,13 +95,9 @@ async fn rocket() -> _ {
     .to_cors()
     .expect("error creating CORS fairing");
 
-
     rocket::build()
-    .mount("/", openapi_get_routes![get_items, get_events])
+    .mount("/", openapi_get_routes![get_events])
     .attach(cors)
     .mount("/swagger", make_swagger_ui(&get_docs()))
+    .manage(AppState {db : db})
 }
-
-// #[get("/items")]
-// async fn main() {
-// }
