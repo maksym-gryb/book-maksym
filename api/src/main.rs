d92e9370ec6c1a8f04c6f2d90a4294397b174eff
maksym-gryb <maksym.gryb@gmail.com>
2025-03-16 21:15:09 -0700

    [WIP] feat(api): loads of stuff in progress right now

--- api/src/main.rs
+++ api/src/main.rs
@@ -4,20 +4,23 @@ mod cors;
 
 use rocket::serde::{json::Json, Deserialize, Serialize};
 
-use rocket::get;
+use rocket::{get, post};
+use rocket::form::{Form, FromForm};
 use rocket::State;
 use rocket_okapi::{openapi, openapi_get_routes};
 use rocket_okapi::swagger_ui::{make_swagger_ui, SwaggerUIConfig};
 use rocket_okapi::okapi::schemars;
 use rocket_okapi::okapi::schemars::JsonSchema;
 
-use sqlx::{migrate::MigrateDatabase, Sqlite, SqlitePool, FromRow};
+// use rocket_auth::{Users, Error, Auth, Signup, Login};
 
-#[derive(Serialize, Deserialize, JsonSchema, Clone, FromRow, Debug)]
+use sqlx::{migrate::MigrateDatabase, Sqlite, SqlitePool, FromRow, Row};
+
+#[derive(Serialize, Deserialize, JsonSchema, Clone, FromRow, Debug, FromForm)]
 struct Event {
-    id: i32,
+    id: Option<i32>,
     title: String,
-    start_date: String,
+    start_date: String,// TODO: find a way to convert to Date/DateTime
     end_date: String
 }
 
@@ -30,13 +33,43 @@ const DB_URL: &str = "sqlite://sqlite.db";
 #[openapi(tag = "Events")]
 #[get("/events")]
 async fn get_events(state : &State<AppState>) -> Json<Vec<Event>> {
-    let users = sqlx::query_as::<_, Event>(
+    let events = sqlx::query_as::<_, Event>(
         "SELECT * FROM events"
     ).fetch_all(&state.db)
     .await
     .unwrap();
 
-    return Json(users);
+    return Json(events);
+}
+
+#[openapi(tag = "Events")]
+#[post("/events", data = "<event>")]
+async fn create_event(state: &State<AppState>, event: Form<Event>)
+    -> Json<Event> {
+    let result = sqlx::query(
+        "INSERT INTO events
+        (title, start_date, end_date)
+        VALUES
+        ($1, $2, $3)
+        RETURNING id"
+    )
+    .bind(&event.title)
+    .bind(&event.start_date)
+    .bind(&event.end_date)
+    .fetch_one(&state.db)
+    .await;
+
+    let id: i32 = match result {
+        Ok(r) => r.get::<i32, &str>("id"),
+        Err(e) => panic!("could not save row: {}", e)
+    };
+
+    Json(Event{
+        id: Some(id),
+        title: event.title.to_string(),
+        start_date: event.start_date.to_string(),
+        end_date: event.end_date.to_string()
+    })
 }
 
 fn get_docs() -> SwaggerUIConfig {
@@ -45,6 +78,23 @@ fn get_docs() -> SwaggerUIConfig {
     ..Default::default()
     }
 }
+
+
+// #[post("/signup", data="<form>")]
+// async fn signup(form: Form<Signup>, auth: Auth<'_>) {
+//     auth.signup(&form).await;
+//     auth.login(&form.into());
+// }
+
+// #[post("/login", data="<form>")]
+// fn login(form: Form<Login>, auth: Auth) {
+//     auth.login(&form);
+// }
+
+// #[get("/logout")]
+// fn logout(auth: Auth) {
+//     auth.logout();
+// }
 
 #[launch]
 #[tokio::main]
@@ -97,8 +147,8 @@ async fn rocket() -> _ {
     // .expect("error creating CORS fairing");
 
     rocket::build()
-    .mount("/", openapi_get_routes![get_events])
-    // .attach(cors)
+    .mount("/", openapi_get_routes![get_events, create_event])
+    // .mount("/", routes![signup, login, logout])
     .attach(cors::CORS)
     .mount("/swagger", make_swagger_ui(&get_docs()))
     .manage(AppState {db : db})
