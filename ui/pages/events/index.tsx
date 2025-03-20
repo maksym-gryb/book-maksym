2428336a9e9dbd35e477d3eb2c38e72184cdbf1a
maksym-gryb <maksym.gryb@gmail.com>
2025-03-19 19:57:14 -0700

    fix(ui): minor type issue

--- ui/pages/events/index.tsx
+++ ui/pages/events/index.tsx
@@ -1,10 +1,16 @@
 // import { useState, useEffect } from "react"
 import useSWR from 'swr'
 
-const fetcher = (...args: any) => fetch(...args, {credentials: 'include'}).then((res) => res.json())
+interface Event {
+    title: string,
+    start_date: string,
+    end_date: string
+}
+
+const fetcher = (...args: [RequestInfo | URL, RequestInit?]) => fetch(args[0], {...args[1], credentials: 'include'}).then((res) => res.json())
 
 export default function Page(){
-    const {data, error} = useSWR("http://localhost:8000/events", fetcher)
+    const {data, error} = useSWR<Event[]>("http://localhost:8000/events", fetcher)
 
     if (!data) return <p>Loading data...</p>
     if (error) return <p>error</p>
@@ -14,7 +20,7 @@ export default function Page(){
             <h1>Events</h1>
             <ul>
                 {data.map((e) => (
-                    <li className="border-solid border-radius-2 border-gray-300">
+                    <li key={e.title + e.start_date + e.end_date} className="border-solid border-radius-2 border-gray-300">
                         <div>{e.title}</div>
                         <div>{e.start_date}</div>
                         <div>to</div>
